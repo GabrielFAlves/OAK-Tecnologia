c642dc23f2870283d8d21e0a9a9b7c2ae8be1748
Gabriel Firmamento <126484540+GabrielFAlves@users.noreply.github.com>
2024-03-25 13:34:59 -0300

    comm

--- src/components/Modal/ProductsModal.jsx
+++ src/components/Modal/ProductsModal.jsx
@@ -4,8 +4,8 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import TextField from '@mui/material/TextField';
-import Checkbox from '@mui/material/Checkbox'; // Importe o Checkbox
-import FormControlLabel from '@mui/material/FormControlLabel'; // Importe o FormControlLabel
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import { useFirebase } from '../../context/Firebase.context'; 
 
 const style = {
@@ -28,27 +28,22 @@ export default function ProductsModal() {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  // Estados para armazenar os valores do formulário
   const [formData, setFormData] = useState({
     id: Math.floor(Math.random() * 1001),
     name: '',
     description: '',
     value: '',
-    available: false, // Inicialize como false
+    available: false,
   });
 
-  // Função para lidar com a alteração nos campos do formulário
   const handleInputChange = (event) => {
     const { name, value, checked, type } = event.target;
-    const newValue = type === 'checkbox' ? checked : value; // Se for um checkbox, pegue o valor de 'checked', senão, use 'value'
+    const newValue = type === 'checkbox' ? checked : value;
     setFormData({ ...formData, [name]: newValue });
   };
 
-  // Função para lidar com o envio do formulário
   const handleSubmit = async () => {
-    // Verificar se todos os campos obrigatórios foram preenchidos
     if (formData.name && formData.description && formData.value !== '') {
-      // Aqui você pode realizar a lógica necessária com os dados do formulário
       await setProduct(formData).then(async () => {
         await fetchDataProducts();
         setFormData({
@@ -56,14 +51,12 @@ export default function ProductsModal() {
           name: '',
           description: '',
           value: '',
-          available: false, // Reinicialize como false
+          available: false,
         });
       });
   
-      // Fechar o modal após o envio do formulário
       handleClose();
     } else {
-      // Se algum campo obrigatório estiver vazio, exiba uma mensagem ou realize outra ação
       alert('Por favor, preencha todos os campos obrigatórios.')
     }
   };
@@ -81,7 +74,6 @@ export default function ProductsModal() {
           <Typography id="modal-modal-title" variant="h6" component="h2">
             Preencha o formulário
           </Typography>
-          {/* Formulário */}
           <form>
             <TextField
               label="Nome do Produto"
@@ -111,7 +103,6 @@ export default function ProductsModal() {
               fullWidth
               margin="normal"
             />
-            {/* Utilize o Checkbox */}
             <FormControlLabel
               control={<Checkbox checked={formData.available} onChange={handleInputChange} name="available" />}
               label="Disponível"
